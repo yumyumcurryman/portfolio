5fb4b4a9c24f84e1bfd26a9225326d58e4199586
yumyumcurryman <micahkelias@gmail.com>
2021-12-03 15:32:57 -0500

    added project grid

---
+++ src/components/projectGridItem.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/projectGridItem.css";
+
+const ProjectGridItem = (props) => {
+  const data = props.data;
+  return (
+    <div className="project-card">
+      <img src={require(`../img/previews/${data.previewImage}`).default} />
+      <h3>{data.title}</h3>
+      <p>{data.description}</p>
+    </div>
+  );
+};
+
+export default ProjectGridItem;
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,9 +1,17 @@
 import React from "react";
+import ProjectGridItem from "../components/projectGridItem";
+import "../styles/projects.css";
+import projectData from "../static/projects.json";
 
 const Projects = () => {
   return (
     <div>
       <h2>Projects</h2>
+      <div className="projects-grid">
+        {projectData.map((data) => (
+          <ProjectGridItem key={data.id} data={data} />
+        ))}
+      </div>
     </div>
   );
 };
