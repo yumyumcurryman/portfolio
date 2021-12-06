c289f4ebb4c4b3d7607ab9a245f47c116fcd7ff1
yumyumcurryman <micahkelias@gmail.com>
2021-12-05 19:50:53 -0500

    added a fuck ton of page stuff

--- src/pages/project.js
+++ src/pages/project.js
@@ -25,9 +25,64 @@ const Project = (props) => {
         <i className="material-icons">navigate_next</i>
         <h2>{project.title}</h2>
       </div>
-      <p>{project.description}</p>
+      <div className="project-content">
+        <div id="project-image-spanner">
+          <img
+            src={require(`../img/previews/${project.previewImage}`).default}
+            alt=""
+          />
+        </div>
+        <h3 className="section-content-header">Description</h3>
+        <div dangerouslySetInnerHTML={{ __html: project.article }}></div>
+        <h3 className="section-content-header">Project Info</h3>
+        <div id="project-info">
+          <BadgeItem description="Date" item={project.date} />
+          <BadgeItem description="Website" item={project.website} />
+          <BadgeItem description="Tech Stack" item={project.stack} />
+        </div>
+      </div>
     </div>
   );
+};
+
+const BadgeItem = (props) => {
+  if (props.item == null) {
+    return null;
+  }
+  if (props.description === "Website") {
+    return (
+      <div className="badge-item">
+        <div className="badge">{props.description}</div>
+        <a href={props.item} target="_blank" rel="noreferrer">
+          {props.item}
+        </a>
+      </div>
+    );
+  }
+  return (
+    <div className="badge-item">
+      <div className="badge">{props.description}</div>
+      {props.description === "Tech Stack" ? (
+        <TechStackRow stack={props.item} />
+      ) : (
+        props.item
+      )}
+    </div>
+  );
+};
+
+const TechStackRow = (props) => {
+  return (
+    <div className="tech-stack-row">
+      {props.stack.map((item) => (
+        <TechStackItem item={item} />
+      ))}
+    </div>
+  );
+};
+
+const TechStackItem = (props) => {
+  return <div className="tech-stack-item">{props.item}</div>;
 };
 
 export default Project;
