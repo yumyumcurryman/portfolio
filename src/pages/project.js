7c07663c152819f54b3a82f74e9ff5a61fe51227
yumyumcurryman <micahkelias@gmail.com>
2021-12-06 15:56:11 -0500

    main projects mostly done

--- src/pages/project.js
+++ src/pages/project.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router";
 import projectData from "../static/projects.json";
 import "../styles/project.css";
 import { NavLink } from "react-router-dom";
+import Modal from "react-modal";
 
 function getProject(id) {
   for (let i = 0; i < projectData.projects.length; i++) {
@@ -15,11 +16,17 @@ function getProject(id) {
   }
 }
 
+Modal.setAppElement("#root");
+Modal.defaultStyles.overlay.backgroundColor = "rgba(34,34,34, 0.95)";
+
 const Project = (props) => {
   let params = useParams();
   let project = getProject(params.projectId);
+  const [showModal, setShowModal] = useState(false);
+  const [activeImage, setActiveImage] = useState("");
+
   return (
-    <div>
+    <div id="project">
       <div id="project-header">
         <NavLink to="/projects">Projects</NavLink>
         <i className="material-icons">navigate_next</i>
@@ -40,6 +47,65 @@ const Project = (props) => {
           <BadgeItem description="Website" item={project.website} />
           <BadgeItem description="Tech Stack" item={project.stack} />
         </div>
+        <MediaGrid
+          id={project.id}
+          images={project.images}
+          modalUpdater={setShowModal}
+          imageUpdater={setActiveImage}
+        />
+        <Modal
+          className="Modal"
+          isOpen={showModal}
+          contentLabel="Minimal Modal Example"
+          onRequestClose={() => setShowModal(false)}
+        >
+          <div className="modal-content">
+            <button id="close-modal" onClick={() => setShowModal(false)}>
+              <i className="material-icons">close</i>
+            </button>
+            <ModalImage imageID={project.id} imageName={activeImage} />
+          </div>
+        </Modal>
+      </div>
+    </div>
+  );
+};
+
+const ModalImage = (props) => {
+  if (props.imageName == null) {
+    return null;
+  }
+  return (
+    <img
+      src={
+        require(`../img/scrollable/${props.imageID}/${props.imageName}`).default
+      }
+      alt=""
+      id="modal-image"
+    />
+  );
+};
+
+const MediaGrid = (props) => {
+  if (props.images == null) {
+    return null;
+  }
+  const buttonHandler = (imageName) => {
+    props.modalUpdater(true);
+    props.imageUpdater(imageName);
+  };
+  return (
+    <div>
+      <h3 className="section-content-header">Media</h3>
+      <div className="media-grid">
+        {props.images.map((image) => (
+          <img
+            src={require(`../img/scrollable/${props.id}/${image}`).default}
+            alt=""
+            onClick={() => buttonHandler(image)}
+            id="project-media-image"
+          />
+        ))}
       </div>
     </div>
   );
@@ -50,12 +116,20 @@ const BadgeItem = (props) => {
     return null;
   }
   if (props.description === "Website") {
+    let isDep = false;
+    if (props.item.includes("deprecated")) {
+      isDep = true;
+    }
     return (
       <div className="badge-item">
         <div className="badge">{props.description}</div>
-        <a href={props.item} target="_blank" rel="noreferrer">
-          {props.item}
-        </a>
+        {isDep ? (
+          props.item
+        ) : (
+          <a href={props.item} target="_blank" rel="noreferrer">
+            {props.item}
+          </a>
+        )}
       </div>
     );
   }
